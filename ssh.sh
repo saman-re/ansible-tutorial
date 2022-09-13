1702eb9e535073a7b1e47bb7e36faecf1ca440f8
saman-re <68374661+saman-re@users.noreply.github.com>
2022-09-13 07:09:23 +0430

    ssh overview video number 2

--- ssh.sh
+++ ssh.sh
@@ -1,2 +1,33 @@
 #! /bin/bash
   
+#need to install Open SSH on the servers
+
+cd ~/.ssh
+
+# -t stand for type
+#-C stand for comment
+ssh-keygen -t ed25519 -C "jay default"
+
+#ssh-keygen ssh_name
+##pass phrase is optional and every time ssh to server must be enter
+#public key can be shared to others
+
+# -i stands for input file 
+ssh-copy-id -i ~/.ssh/ssh-file-name.pub server_ip
+
+#enter password and done!
+
+#to ssh --> ssh server-ip
+
+#ssh using private key
+ssh -i ~/.ssh/ansible server_ip
+
+#eval $(ssh-agent)
+#ssh-add
+ 
+##using alias
+
+alias ssha="eval $(ssh-agent) && ssh-add"
+
+#closing terminal will clear the alias
+#for permenant alias change .bashrc file
